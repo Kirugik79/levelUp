84fd2e978ce7297eac276124062c53d6ff7ea987
Kirugik79 <rkirui84@gmail.com>
2018-06-27 18:31:23 +0300

    commandline login

---
+++ day3/login.py
@@ -0,0 +1,23 @@
+
+username = "Robert"
+
+password = 456
+
+attempt = 0
+
+while attempt !=3:
+    username = input ("Enter Username: ")
+
+    if username != username:
+        print ("Incorrect username")
+
+    else:
+        password = input ("Enter Password: ")
+        if password == password:
+            print ("Welcome " + username)
+            break
+        else:
+            print ("Welcome " + username)
+            attempt = attempt +1
+            if attempt == 3:
+                    print ("You have reached maximum login attempts")
